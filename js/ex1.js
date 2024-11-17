da6599e1200919ed9fc644ea885d446623d29e28
tisha-islam <tishaweb321@gmail.com>
2024-11-17 14:24:11 +0600

    first commit

---
+++ js/ex1.js
@@ -0,0 +1,8 @@
+document.getElementById("student_message").innerHTML="Hi,my name is Tisha Islam,I am 20 years old and learning JavaScript";
+
+var num_1=101;
+var num_2=7;
+var division = num_1/num_2
+console.log(division.toFixed(2));
+
+document.getElementById("result").innerHTML=division.toFixed(2)
